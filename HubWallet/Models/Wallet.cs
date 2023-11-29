5c8d481cd5f898ed3094148c2bb24e3a0a4b45a6
Divine Sackitey <sackiteydivine24@gmail.com>
2023-11-29 12:21:04 +0000

    implemented wallet api functionality

---
+++ HubWallet/Models/Wallet.cs
@@ -0,0 +1,13 @@
+﻿namespace HubWallet.Models
+{
+    public class Wallet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string AccountNumber {  get; set; }
+        public string AccountScheme {  get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Owner {  get; set; }
+    }
+}
